f88c74666ff736c67648032bc088f18f2be1bfe9
Robert Hardege <RobertHardege@googlemail.com>
2024-08-04 13:58:29 +0200

    update to marlin 2.1.2.4

--- Marlin/src/lcd/menu/menu_language.cpp
+++ Marlin/src/lcd/menu/menu_language.cpp
@@ -1,33 +1,59 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+//
+// Language Selection Menu
+//
+
 #include "../../inc/MarlinConfig.h"
+
+#if HAS_MENU_MULTI_LANGUAGE
+
+#include "menu_item.h"
 #include "../../MarlinCore.h"
-#include "../../module/configuration_store.h"
+#include "../../module/settings.h"
 
-#include "menu.h"
-
-uint8_t lang = 0;
-uint8_t language_change_font;
-static void set_select_font_english_to_eeprom()
-{
-  language_change_font = 0;
-  lang = language_change_font;
-  (void)settings.save();
-  ui.refresh();
-}
-static void set_select_font_chinese_to_eeprom()
-{
-  language_change_font = 1;
-  lang = language_change_font;
-  (void)settings.save();
-  ui.refresh();
+static void set_lcd_language(const uint8_t inlang) {
+  ui.set_language(inlang);
+  TERN_(LCD_LANGUAGE_AUTO_SAVE, (void)settings.save());
 }
 
-void menu_language()
-{
+void menu_language() {
   START_MENU();
-  BACK_ITEM(MSG_MAIN);
+  BACK_ITEM(MSG_MAIN_MENU);
 
-  MENU_ITEM(function, MSG_ENGLISH_LANGUAGE, set_select_font_english_to_eeprom);
-  MENU_ITEM(function, MSG_CHINESE_LANGUAGE, set_select_font_chinese_to_eeprom);
+  MENU_ITEM_F(function, FPSTR(GET_LANGUAGE_NAME(1)), []{ set_lcd_language(0); });
+  MENU_ITEM_F(function, FPSTR(GET_LANGUAGE_NAME(2)), []{ set_lcd_language(1); });
+  #if NUM_LANGUAGES > 2
+    MENU_ITEM_F(function, FPSTR(GET_LANGUAGE_NAME(3)), []{ set_lcd_language(2); });
+    #if NUM_LANGUAGES > 3
+      MENU_ITEM_F(function, FPSTR(GET_LANGUAGE_NAME(4)), []{ set_lcd_language(3); });
+      #if NUM_LANGUAGES > 4
+        MENU_ITEM_F(function, FPSTR(GET_LANGUAGE_NAME(5)), []{ set_lcd_language(4); });
+      #endif
+    #endif
+  #endif
 
   END_MENU();
 }
+
+#endif // HAS_MENU_MULTI_LANGUAGE
